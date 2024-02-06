5771d6607e50501f64cb874eb58e4c8da512420d
Tajkamal <158159691+Tajkamal1@users.noreply.github.com>
2024-02-06 08:38:54 +0530

    Delete src directory

--- src/models/blogModel.js
+++
@@ -1,9 +0,0 @@
-// models/blogModel.js
-const mongoose = require('mongoose');
-
-const BlogData = mongoose.model('BlogData', {
-  title: String,
-  content: String,
-});
-
-module.exports = BlogData;
